5a3a5d7461069dcc93714af087ca7e4554aad694
TANADADaisuke <nvkx873110@gmail.com>
2021-11-29 19:34:20 +0900

    chore: temp commit

--- app.py
+++ app.py
@@ -2,26 +2,7 @@ import glob
 import os
 import sys
 from PIL import Image
-# im = Image.open('media/IMG_2934.JPG')
 
-# you can now use instance attributes to examine the file content
-# print(im.format, im.size, im.mode)
-
-# let's display the image; we can use instance methods
-# im.show()
-
-# thumbnail
-# size = (128, 128)
-# im.thumbnail(size)
-# im.save('media/IMG_2934.thumbnail', 'JPEG')
-
-# this is a simple script to quickly identify a set of image files:
-# for infile in sys.argv[1:]:
-#     try:
-#         with Image.open(infile) as im:
-#             print(infile, im.format, f'{im.size}x{im.mode}')
-#     except OSError:
-#         pass
 
 # get file size
 files = glob.glob('media/*.*')
@@ -42,4 +23,14 @@ print(files[0], ": ", file_size)
 #             out.save(outfile)
 #     except OSError:
 #         pass
+
+# this is a simple script to quickly identify a set of image files:
+# for infile in sys.argv[1:]:
+#     try:
+#         with Image.open(infile) as im:
+#             print(infile, im.format, f'{im.size}x{im.mode}')
+#     except OSError:
+#         pass
+
+
 
