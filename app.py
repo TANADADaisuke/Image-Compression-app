701907963d832a6ee8a044609e345c5d871d39f8
TANADADaiske <nvkx873110@gmail.com>
2021-11-30 21:17:28 +0900

    feat:Create peudocode

--- app.py
+++ app.py
@@ -4,12 +4,38 @@ import sys
 from PIL import Image
 
 
+# set variables
+COMPRESE_RATE = 1.5
+COMPRESS_STEP = 0.5
+MEDIA_DIR = 'media'
+
 # get file size
 files = glob.glob('media/*.*')
 file_size = os.stat(files[0]).st_size
 print(files[0], ": ", file_size)
 
 
+def compress_files():
+    """
+    compress all files in MEDIA_DIR.
+
+    Return: None
+    """
+    files = get_files()
+    pass
+# this is a simple script to quickly identify a set of image files:
+# for infile in sys.argv[1:]:
+#     try:
+#         with Image.open(infile) as im:
+#             print(infile, im.format, f'{im.size}x{im.mode}')
+#     except OSError:
+#         pass
+
+def compress_given_file(file, rate):
+    """
+    compress a given file with given rate
+    """
+    pass
 # Geometrical transform:
 # compose_rate = 3
 # for infile in sys.argv[1:]:
@@ -24,13 +50,23 @@ print(files[0], ": ", file_size)
 #     except OSError:
 #         pass
 
-# this is a simple script to quickly identify a set of image files:
-# for infile in sys.argv[1:]:
-#     try:
-#         with Image.open(infile) as im:
-#             print(infile, im.format, f'{im.size}x{im.mode}')
-#     except OSError:
-#         pass
+def get_files():
+    pass
+
+def main():
+    """
+    main function
+    """
+    # in case with sys args
+    if len(sys.args) == 2:
+        compress_given_file(sys.args[1], sys.args[2])
+    elif len(sys.args) == 1:
+        compress_files()
+    else:
+        print(
+            'Causion! App should run with no args or only with ' + \
+            'one pair of file and compress rate.'
+        )
 
 
 
